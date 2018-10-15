9bb47831f167b7f5251e7235f9af4e148d2664d4
Stefan Löffler <st.loeffler@gmail.com>
2018-10-15 19:56:45 +0200

    Add debug info

--- ci/appveyor/build-hunspell.sh
+++ ci/appveyor/build-hunspell.sh
@@ -2,9 +2,22 @@
 
 ARCHIVE=v1.6.2.tar.gz
 
+echo "OK1"
 mkdir -p /c/projects/hunspell
 cd /c/projects/hunspell
+
+echo "OK2"
+pwd
+ls
+
+
 curl -sS -O https://github.com/hunspell/hunspell/archive/$ARCHIVE
+
+echo "OK3"
+pwd
+ls
+
 tar -xvf $ARCHIVE
 
+echo "OK4"
 ls
