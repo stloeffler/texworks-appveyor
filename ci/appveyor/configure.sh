ca82539f15bea3ecda1a5fe02da9c241644bdbf6
Stefan Löffler <st.loeffler@gmail.com>
2018-10-16 20:41:03 +0200

    Add some informational text and debug output

--- ci/appveyor/configure.sh
+++ ci/appveyor/configure.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+echo "Configuring TeXworks"
+
 mkdir -p "${APPVEYOR_BUILD_FOLDER}/build"
 cd "${APPVEYOR_BUILD_FOLDER}/build"
 
+
+
+echo "Copying poppler-data"
 # Extract poppler-data (required by some tests)
 mkdir -p share
+echo "cp -r /c/projects/poppler-data/${popplerdata_DIRNAME} share/poppler"
 cp -r /c/projects/poppler-data/${popplerdata_DIRNAME} share/poppler
 
 # FIXME: DEBUG
+echo "${popplerdata_DIRNAME}"
+echo "ls -l share/poppler"
 ls -l share/poppler
 
 cmake -G"MSYS Makefiles" -DTW_BUILD_ID='appveyor' -DDESIRED_QT_VERSION='5' -DCMAKE_BUILD_TYPE='Release' -DTEXWORKS_ADDITIONAL_LIBS="shlwapi" -DWITH_LUA=OFF ..
--- ci/appveyor/get-dependencies.sh
+++ ci/appveyor/get-dependencies.sh
@@ -19,32 +19,40 @@ popplerdata_URL=https://poppler.freedesktop.org/${popplerdata_ARCHIVE}
 
 pacman --noconfirm -S mingw-w64-i686-freetype mingw-w64-i686-openjpeg2 mingw-w64-i686-lcms2 mingw-w64-i686-libpng mingw-w64-i686-libtiff mingw-w64-i686-curl mingw-w64-i686-lua
 
+echo "Downloading poppler-data"
 mkdir -p /c/projects/poppler-data
 cd /c/projects/poppler-data
 curl -sSL -O ${popplerdata_URL}
 # FIXME: Check checksum
+echo "Extracting poppler-data"
 7z x "${popplerdata_ARCHIVE}" -so | 7z x -si -ttar
 
 
+echo "Downloading poppler"
 mkdir -p /c/projects/poppler
 cd /c/projects/poppler
-
 curl -sSL -O ${poppler_URL}
 # FIXME: Check checksum
+echo "Extracting poppler"
 7z x "${poppler_ARCHIVE}" -so | 7z x -si -ttar
 cd ${poppler_DIRNAME}
+echo "Patching poppler"
 for PATCH in $(find ${APPVEYOR_BUILD_FOLDER}/travis-ci/mxe/ -iname 'poppler-*.patch'); do
 	echo "Applying ${PATCH}"
 	patch -p1 < "${PATCH}"
 done
+echo "Building poppler"
 mkdir build && cd build && cmake -G"MSYS Makefiles" -DCMAKE_BUILD_TYPE="Release" -DBUILD_QT5_TESTS=OFF -DENABLE_CPP=OFF -DENABLE_UTILS=OFF -DENABLE_XPDF_HEADERS=ON -DCMAKE_INSTALL_PREFIX="/mingw32" .. && make && make install
 
 
+echo "Downloading hunspell"
 mkdir -p /c/projects/hunspell
 cd /c/projects/hunspell
 curl -sSL -O ${hunspell_URL}
 # FIXME: Check checksum
+echo "Extracting hunspell"
 7z x "${hunspell_ARCHIVE}" -so | 7z x -si -ttar
+echo "Building hunspell"
 cd ${hunspell_DIRNAME}
 autoreconf -i && ./configure && make && make install
 
