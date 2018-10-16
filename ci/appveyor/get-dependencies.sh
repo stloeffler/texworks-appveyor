c94fea49fb1aa31b4f33e366a1bef9869820ab3e
Stefan Löffler <st.loeffler@gmail.com>
2018-10-16 20:49:50 +0200

    Export lib vars

--- ci/appveyor/get-dependencies.sh
+++ ci/appveyor/get-dependencies.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-hunspell_VERSION=1.6.2
-hunspell_ARCHIVE=v${hunspell_VERSION}.tar.gz
-hunspell_DIRNAME=hunspell-${hunspell_VERSION}
-hunspell_URL=https://github.com/hunspell/hunspell/archive/$hunspell_ARCHIVE
+export hunspell_VERSION=1.6.2
+export hunspell_ARCHIVE=v${hunspell_VERSION}.tar.gz
+export hunspell_DIRNAME=hunspell-${hunspell_VERSION}
+export hunspell_URL=https://github.com/hunspell/hunspell/archive/$hunspell_ARCHIVE
 
-poppler_VERSION=0.64.0
-poppler_ARCHIVE=poppler-${poppler_VERSION}.tar.xz
-poppler_DIRNAME=poppler-${poppler_VERSION}
-poppler_URL=https://poppler.freedesktop.org/${poppler_ARCHIVE}
+export poppler_VERSION=0.64.0
+export poppler_ARCHIVE=poppler-${poppler_VERSION}.tar.xz
+export poppler_DIRNAME=poppler-${poppler_VERSION}
+export poppler_URL=https://poppler.freedesktop.org/${poppler_ARCHIVE}
 
 
-popplerdata_VERSION=0.4.9
-popplerdata_ARCHIVE=poppler-data-${popplerdata_VERSION}.tar.gz
-popplerdata_DIRNAME=poppler-data-${popplerdata_VERSION}
-popplerdata_URL=https://poppler.freedesktop.org/${popplerdata_ARCHIVE}
+export popplerdata_VERSION=0.4.9
+export popplerdata_ARCHIVE=poppler-data-${popplerdata_VERSION}.tar.gz
+export popplerdata_DIRNAME=poppler-data-${popplerdata_VERSION}
+export popplerdata_URL=https://poppler.freedesktop.org/${popplerdata_ARCHIVE}
 
 
 pacman --noconfirm -S mingw-w64-i686-freetype mingw-w64-i686-openjpeg2 mingw-w64-i686-lcms2 mingw-w64-i686-libpng mingw-w64-i686-libtiff mingw-w64-i686-curl mingw-w64-i686-lua
