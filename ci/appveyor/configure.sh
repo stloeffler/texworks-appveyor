c5e4cef70e3714cceb86870c6a596d381fad7753
Stefan Löffler <st.loeffler@gmail.com>
2018-10-16 07:03:32 +0200

    Try to link against shlwapi

--- ci/appveyor/configure.sh
+++ ci/appveyor/configure.sh
@@ -3,4 +3,4 @@
 mkdir -p "${APPVEYOR_BUILD_FOLDER}/build"
 cd "${APPVEYOR_BUILD_FOLDER}/build"
 
-cmake -G"MSYS Makefiles" -DTW_BUILD_ID='appveyor' -DDESIRED_QT_VERSION='5' -DCMAKE_BUILD_TYPE='Release' ..
+cmake -G"MSYS Makefiles" -DTW_BUILD_ID='appveyor' -DDESIRED_QT_VERSION='5' -DCMAKE_BUILD_TYPE='Release' -DTEXWORKS_ADDITIONAL_LIBS="shlwapi" ..
