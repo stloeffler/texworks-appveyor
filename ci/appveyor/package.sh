5f3c0f167d4b12a842cc3a730fe2616b6c1b7210
Stefan Löffler <st.loeffler@gmail.com>
2019-07-20 10:00:10 +0200

    Strip artifact and force windeployqt to use release libs

--- ci/appveyor/package.sh
+++ ci/appveyor/package.sh
@@ -6,9 +6,12 @@ cd "${APPVEYOR_BUILD_FOLDER}/build"
 
 make VERBOSE=1 install
 
-windeployqt "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
+strip -s "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
+
+windeployqt--release "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
 
 cp -r share "${APPVEYOR_BUILD_FOLDER}/artifact/"
 
 # FIXME: DEBUG
 ls -lisaR "${APPVEYOR_BUILD_FOLDER}/artifact/"
+du -hd 1 "${APPVEYOR_BUILD_FOLDER}/artifact/"
