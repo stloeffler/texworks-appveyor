b82480ed3aaaa94de4180bac1f534afd21f81eab
Stefan Löffler <st.loeffler@gmail.com>
2019-07-20 11:46:35 +0200

    Move package versions to separate file
    so they can easily be referenced in multiple scripts

--- ci/appveyor/get-dependencies.sh
+++ ci/appveyor/get-dependencies.sh
@@ -1,22 +1,7 @@
 #!/bin/sh
 
 . ${APPVEYOR_BUILD_FOLDER}/ci/appveyor/defs.sh
-
-hunspell_VERSION=1.7.0
-hunspell_ARCHIVE=v${hunspell_VERSION}.tar.gz
-hunspell_DIRNAME=hunspell-${hunspell_VERSION}
-hunspell_URL=https://github.com/hunspell/hunspell/archive/$hunspell_ARCHIVE
-
-poppler_VERSION=0.79.0
-poppler_ARCHIVE=poppler-${poppler_VERSION}.tar.xz
-poppler_DIRNAME=poppler-${poppler_VERSION}
-poppler_URL=https://poppler.freedesktop.org/${poppler_ARCHIVE}
-
-
-popplerdata_VERSION=0.4.9
-popplerdata_ARCHIVE=poppler-data-${popplerdata_VERSION}.tar.gz
-popplerdata_DIRNAME=poppler-data-${popplerdata_VERSION}
-popplerdata_URL=https://poppler.freedesktop.org/${popplerdata_ARCHIVE}
+. ${APPVEYOR_BUILD_FOLDER}/ci/appveyor/package_versions.sh
 
 
 print_headline "Installing dependencies"
