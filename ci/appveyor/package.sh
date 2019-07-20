9a6b94ae532803de3e14b5beda40779bdc3da8a1
Stefan Löffler <st.loeffler@gmail.com>
2019-07-20 11:47:07 +0200

    Copy additional dlls (hunspell, poppler, zlib)

--- ci/appveyor/package.sh
+++ ci/appveyor/package.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "Packaging TeXworks"
+. ${APPVEYOR_BUILD_FOLDER}/ci/appveyor/defs.sh
+. ${APPVEYOR_BUILD_FOLDER}/ci/appveyor/package_versions.sh
+
+print_headline "Packaging TeXworks"
 
 cd "${APPVEYOR_BUILD_FOLDER}/build"
 
@@ -8,8 +11,18 @@ make VERBOSE=1 install
 
 strip -s "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
 
+# Copy Qt dlls
 windeployqt --release "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
 
+# Copy Hunspell dll
+cp /mingw64/lib/../bin/libhunspell-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+
+# Copy Poppler dlls
+cp /mingw64/bin/libpoppler-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+
+cp /mingw64/bin/zlib1.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+
+# Copy poppler data
 cp -r share "${APPVEYOR_BUILD_FOLDER}/artifact/"
 
 # FIXME: DEBUG
