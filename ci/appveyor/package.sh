4f513a6be46167f7277ab07b5d2db8dcf684df00
Stefan Löffler <st.loeffler@gmail.com>
2019-07-20 16:14:22 +0200

    Try to resolve all dll dependencies using a python script

--- ci/appveyor/package.sh
+++ ci/appveyor/package.sh
@@ -14,20 +14,22 @@ strip -s "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
 # Copy Qt dlls
 windeployqt --release "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
 
-# Copy Hunspell dll
-cp /mingw64/lib/../bin/libhunspell-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+## Copy Hunspell dll
+#cp /mingw64/lib/../bin/libhunspell-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#
+## Copy Poppler dlls
+#cp /mingw64/bin/libpoppler-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#
+#cp /mingw64/bin/zlib1.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#cp /mingw64/bin/libcurl-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#cp /mingw64/bin/libfreetype-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#cp /mingw64/bin/libjpeg-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#cp /mingw64/bin/liblcms2-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#cp /mingw64/bin/libpng16-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#cp /mingw64/bin/libtiff-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+#cp /mingw64/bin/libopenjp2-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
 
-# Copy Poppler dlls
-cp /mingw64/bin/libpoppler-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-
-cp /mingw64/bin/zlib1.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-cp /mingw64/bin/libcurl-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-cp /mingw64/bin/libfreetype-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-cp /mingw64/bin/libjpeg-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-cp /mingw64/bin/liblcms2-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-cp /mingw64/bin/libpng16-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-cp /mingw64/bin/libtiff-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-cp /mingw64/bin/libopenjp2-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+python ${APPVEYOR_BUILD_FOLDER}/ci/appveyor/resolve-dlls.py "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
 
 # Copy poppler data
 cp -r share "${APPVEYOR_BUILD_FOLDER}/artifact/"
