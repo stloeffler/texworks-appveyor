6765f6064a8a98aa7639cd44f1ec07ea32d5f339
Stefan Löffler <st.loeffler@gmail.com>
2019-07-21 20:06:07 +0200

    Fix install and dll paths + misc cleanup

--- ci/appveyor/package.sh
+++ ci/appveyor/package.sh
@@ -9,29 +9,15 @@ cd "${APPVEYOR_BUILD_FOLDER}/build"
 
 make VERBOSE=1 install
 
-strip -s "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
+strip -s "${APPVEYOR_BUILD_FOLDER}/artifact/TeXworks.exe"
 
 # Copy Qt dlls
-windeployqt --release "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
-
-## Copy Hunspell dll
-#cp /mingw64/lib/../bin/libhunspell-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#
-## Copy Poppler dlls
-#cp /mingw64/bin/libpoppler-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#
-#cp /mingw64/bin/zlib1.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#cp /mingw64/bin/libcurl-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#cp /mingw64/bin/libfreetype-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#cp /mingw64/bin/libjpeg-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#cp /mingw64/bin/liblcms2-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#cp /mingw64/bin/libpng16-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#cp /mingw64/bin/libtiff-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
-#cp /mingw64/bin/libopenjp2-*.dll "${APPVEYOR_BUILD_FOLDER}/artifact/bin/"
+windeployqt --release "${APPVEYOR_BUILD_FOLDER}/artifact/TeXworks.exe"
 
 print_info "Resolving DLL Dependencies"
 
-python ${APPVEYOR_BUILD_FOLDER}/ci/appveyor/resolve-dlls.py "${APPVEYOR_BUILD_FOLDER}/artifact/bin/TeXworks.exe"
+python ${APPVEYOR_BUILD_FOLDER}/ci/appveyor/resolve-dlls.py "${APPVEYOR_BUILD_FOLDER}/artifact/TeXworks.exe"
+python ${APPVEYOR_BUILD_FOLDER}/ci/appveyor/resolve-dlls.py "${APPVEYOR_BUILD_FOLDER}/artifact/libTWLuaPlugin.dll"
 
 # Copy poppler data
 cp -r share "${APPVEYOR_BUILD_FOLDER}/artifact/"
