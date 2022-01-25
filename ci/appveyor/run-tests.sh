99f5192e65d80a18046f3536e85c6c3fc20be1fa
Stefan Löffler <st.loeffler@gmail.com>
2022-01-25 22:13:27 +0100

    Copy poppler dlls before running tests

--- ci/appveyor/run-tests.sh
+++ ci/appveyor/run-tests.sh
@@ -2,4 +2,8 @@
 
 cd "${APPVEYOR_BUILD_FOLDER}/build"
 
+# Copy poppler dll to here it is looking for relevant data (e.g., poppler-data,
+# fonts) relative to its location
+cp /mingw64/bin/libpoppler*.dll .
+
 ctest -V
