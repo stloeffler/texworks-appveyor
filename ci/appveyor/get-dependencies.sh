254cd54804352a2d71eb55291177fe170d19577c
Stefan Löffler <st.loeffler@gmail.com>
2024-04-07 15:33:22 +0200

    Move hunspell in front of poppler

--- ci/appveyor/get-dependencies.sh
+++ ci/appveyor/get-dependencies.sh
@@ -14,6 +14,19 @@ pacman --noconfirm -S autotools mingw-w64-x86_64-boost mingw-w64-x86_64-freetype
 # It is required for verifying https connections
 # FIXME: As a temporary workaround, use --insecure with curl
 
+print_headline "Installing hunspell"
+print_info "Downloading hunspell"
+mkdir -p /c/projects/hunspell
+cd /c/projects/hunspell
+curl -sSL -O --insecure "${hunspell_URL}"
+# FIXME: Check checksum
+print_info "Extracting hunspell"
+7z x "${hunspell_ARCHIVE}" -so | 7z x -si -ttar
+print_info "Building hunspell"
+cd "${hunspell_DIRNAME}"
+autoreconf --install --force && ./configure && make -j && make install
+
+
 print_headline "Installing poppler"
 print_info "Downloading poppler"
 mkdir -p /c/projects/poppler
@@ -32,14 +45,3 @@ print_info "Building poppler"
 mkdir build && cd build && cmake -G"MSYS Makefiles" -DCMAKE_BUILD_TYPE="Release" -DBUILD_QT6_TESTS=OFF -DENABLE_QT5=OFF -DENABLE_CPP=OFF -DENABLE_UTILS=OFF -DENABLE_UNSTABLE_API_ABI_HEADERS=ON -DENABLE_RELOCATABLE=ON -DENABLE_GPGME=OFF -DCMAKE_INSTALL_PREFIX="/mingw64" .. && make -j && make install
 
 
-print_headline "Installing hunspell"
-print_info "Downloading hunspell"
-mkdir -p /c/projects/hunspell
-cd /c/projects/hunspell
-curl -sSL -O --insecure "${hunspell_URL}"
-# FIXME: Check checksum
-print_info "Extracting hunspell"
-7z x "${hunspell_ARCHIVE}" -so | 7z x -si -ttar
-print_info "Building hunspell"
-cd "${hunspell_DIRNAME}"
-autoreconf --install --force && ./configure && make -j && make install
